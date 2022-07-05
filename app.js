3a74f0a5cb9381445f5e9863d45f12cee238f54a
IgnacioJC <igncanton.dev@gmail.com>
2022-07-05 11:54:20 -0300

    DOM Interaction, displaying items added

--- app.js
+++ app.js
@@ -43,9 +43,41 @@ const iniciarApp = () => {
         }
     }
 
+    //Imprimir listado en pantalla
+    const printProductos = () => {
+        const arrayDeProductos = () => {
+            //lee propiedades del objeto listaDeItems y crea sus respectivos elementos
+            for (let i = 0; i < listaDeItems.length; i++) {
+                const listaContainer = document.getElementById('lista')
+                const productoItem = document.createElement('div')
+                productoItem.innerHTML = `<p>${listaDeItems[i].nombre}</p>
+                <p>${listaDeItems[i].stock}</p>`
+
+                listaContainer.append(productoItem)
+            }
+        }
+
+        //crea al div contenedor y la estructura en general
+        const contenedor = document.createElement('main')
+        contenedor.innerHTML = `
+        <div class="contenedor">
+            <h1 class="titulo">Lista de Productos</h1>
+            <div class="categorias">
+                <p>Productos</p>
+                <p>Unidades</p>
+            </div>
+            <div id="lista">
+            </div>
+            <button class="button" onclick="document.location.reload(true)">Reiniciar App</button>
+        </div>
+        `
+        document.body.append(contenedor)
+        arrayDeProductos()
+    }
+
     //Consulta que operación se desea realizar
     const consultaOperacion = () => {
-        const userInput = parseInt(prompt("Ingrese el numero de la operación a realizar: 1-Consultar Stock 2-Ingresar Producto 3-Eliminar Producto 4-Terminar Operación"))
+        const userInput = parseInt(prompt("Ingrese el numero de la operación a realizar: 1-Consultar Stock 2-Ingresar Producto 3-Eliminar Producto 4-Mostrar en Pantalla 5-Terminar Operación"))
 
         switch (userInput) {
             case 1:
@@ -94,9 +126,20 @@ const iniciarApp = () => {
                 break;
 
             case 4:
+                //imprimir en pantalla la lista de productos
+                if (listaDeItems.length === 0) {
+                    alert("no products added yet! add some products!")
+                    consultaOperacion()
+                } else {
+                    printProductos()
+                }
+                break;
+
+            case 5:
                 //terminar operación
                 alert("Operación terminada, sistemas apagados, que tenga un buen dia!")
                 break;
+
 
             default:
                 alert("Ingrese un número de operación válido por favor!")
